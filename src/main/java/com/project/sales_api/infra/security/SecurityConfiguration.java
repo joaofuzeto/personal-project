2613054ebff5b6b28d4f8d381f6617d3ea2ae54f
joaofuzeto <fuzeto.jf@gmail.com>
2026-03-05 12:35:27 -0300

    Updating CORS config

--- src/main/java/com/project/sales_api/infra/security/SecurityConfiguration.java
+++ src/main/java/com/project/sales_api/infra/security/SecurityConfiguration.java
@@ -53,18 +53,4 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-
 }
