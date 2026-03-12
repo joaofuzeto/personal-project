7793331518fbf04ad878e7f7d2f18e36076981ee
joaofuzeto <fuzeto.jf@gmail.com>
2026-03-12 04:23:23 -0200

    Fixing problem with annotation

--- src/main/java/com/project/sales_api/controller/CustomerController.java
+++ src/main/java/com/project/sales_api/controller/CustomerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@ SecurityRequirement(name = "bearerAuth")
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/v1/customers")
 public class CustomerController {
